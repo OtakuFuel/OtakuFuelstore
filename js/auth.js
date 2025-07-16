47521d7a5816ba6664b4ecd66521810eac9fec34
OtakuFuel <gujjardeepak441@gmail.com>
2025-07-16 11:10:50 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,15 @@
+document.getElementById("signupForm")?.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = this.name.value;
+  const email = this.email.value;
+  const password = this.password.value;
+
+  localStorage.setItem("name", name);
+  localStorage.setItem("email", email);
+  localStorage.setItem("password", password);
+  localStorage.setItem("isLoggedIn", "false");
+
+  alert("Signup successful! Please login.");
+  window.location.href = "login.html";
+});
